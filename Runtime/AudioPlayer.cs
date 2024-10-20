f068655460b28eb2c8a07d7972d0bc2340a5a1f6
TGerritsen98 <vanaest@outlook.com>
2024-10-20 16:17:10 +0200

    Updated the audio package to use events

--- Runtime/AudioPlayer.cs
+++ Runtime/AudioPlayer.cs
@@ -9,7 +9,7 @@ namespace Incantium.Audio
     /// Class that handles the creation of different audio tracks that can fade audio between each other.
     /// </summary>
     [RequireComponent(typeof(AudioSource))]
-    public sealed class AudioPlayer : SingleBehaviour<AudioPlayer>
+    internal sealed class AudioPlayer : MonoBehaviour
     {
         private const int TRACK_AMOUNT = 2;
         
@@ -21,18 +21,20 @@ namespace Incantium.Audio
         [Tooltip("True if the current audio clip should be played at wake-up.")]
         private bool playOnAwake;
 
+        [SerializeField]
+        [Tooltip("The inputs to the audio player to which it listens.")]
+        private AudioInput input;
+
         private readonly Queue<AudioTrack> tracks = new();
         private AudioSource sfx;
         private Coroutine routine;
 
         /// <summary>
         /// Method called when initializing the audio player. This method will create new <see cref="AudioTrack"/>s to
-        /// play music on. At least two is required for a cross fade effect.
+        /// play music on. At least two is required for a cross-fade effect.
         /// </summary>
-        private new void Awake()
+        private void Awake()
         {
-            base.Awake();
-
             sfx = GetComponent<AudioSource>();
 
             for (var i = 1; i <= TRACK_AMOUNT; i++)
@@ -62,7 +64,17 @@ namespace Incantium.Audio
             
             Play(0, FadeType.CrossFade);
         }
-        
+
+        /// <summary>
+        /// Method to subscribe itself to the inputs.
+        /// </summary>
+        private void OnEnable() => input.player = this;
+
+        /// <summary>
+        /// Method to unsubscribe itself to the inputs.
+        /// </summary>
+        private void OnDisable() => input.player = null;
+
         /// <summary>
         /// Method called when the game is pausing or resuming.
         /// </summary>
@@ -80,7 +92,7 @@ namespace Incantium.Audio
         /// will mean instant transition.</param>
         /// <param name="type">How to fade the audio clip between each other.</param>
         /// <seealso cref="Stop"/>
-        public void Play(float fadeSeconds, FadeType type) => Play(music, fadeSeconds, type);
+        internal void Play(float fadeSeconds, FadeType type) => Play(music, fadeSeconds, type);
         
         /// <summary>
         /// Method to start playing a new audio clip.
@@ -90,7 +102,7 @@ namespace Incantium.Audio
         /// will mean instant transition.</param>
         /// <param name="type">How to fade the audio clip between each other.</param>
         /// <seealso cref="Stop"/>
-        public void Play(MusicClip music, float fadeSeconds, FadeType type)
+        internal void Play(MusicClip music, float fadeSeconds, FadeType type)
         {
             if (!music) throw new ArgumentException("Missing required audio music.");
             if (this.music == music) return;
@@ -111,26 +123,26 @@ namespace Incantium.Audio
         /// Method to play the audio clip as a sound effect once in the background.
         /// </summary>
         /// <param name="music">The audio clip to play.</param>
-        public void PlaySFX(MusicClip music) => sfx.PlayOneShot(music.clip, music.volume);
+        internal void PlaySFX(MusicClip music) => sfx.PlayOneShot(music.clip, music.volume);
         
         /// <summary>
         /// Method to stop the audio player.
         /// </summary>
         /// <param name="fadeSeconds">The amount of seconds to fade out the audio player.</param>
         /// <seealso cref="Play(float,Incantium.Audio.FadeType)"/>
-        public void Stop(float fadeSeconds = 0) => tracks.Peek().Stop(fadeSeconds);
+        internal void Stop(float fadeSeconds = 0) => tracks.Peek().Stop(fadeSeconds);
         
         /// <summary>
         /// Method to pause the audio player.
         /// </summary>
         /// <seealso cref="Resume"/>
-        public void Pause() => tracks.Peek().Pause();
+        internal void Pause() => tracks.Peek().Pause();
 
         /// <summary>
         /// Method to resume the audio player.
         /// </summary>
         /// <seealso cref="Pause"/>
-        public void Resume() => tracks.Peek().Resume();
+        internal void Resume() => tracks.Peek().Resume();
         
         /// <summary>
         /// Method to load in a music clip into local memory. This method will wait until this process has been
